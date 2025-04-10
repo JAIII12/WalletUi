49f310914720d3f3b02ce7bcf8d5192e400ee2cc
JAI <sy9534411@gmail.com>
2025-04-10 16:57:49 +0530

    second commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:walletui/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,15 +13,76 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[300],
-      body: Column(children: [
-        //app bar
+      body: SafeArea(
+        child: Column(children: [
+          //app bar
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 25.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  children: [
+                    Text("My ",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold)
+                ),
+                Text(
+                  'cards',
+                  style: TextStyle(fontSize: 28),
+                ),
+                  ],
+                ),
 
-        //cards
+                //plus button
+                Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color:Colors.grey[400], 
+                    shape: BoxShape.circle),
+                  child: Icon(Icons.add)),
+              ],
+            ),
+          ),
 
-        //3 buttons -> send //pay // bill
+          SizedBox(height: 25),
 
-        //column-> stats +transaction
-      ],),
+          //cards
+      Container(
+      height: 200,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: [
+            MyCard(
+              balance: 5250.20,
+              cardNumber: 123456,
+              expiryMonth: 10,
+              expiryYear: 25,
+              color: Colors.deepPurple[400],
+            ),
+            MyCard(
+              balance: 325.50,
+              cardNumber: 145456,
+              expiryMonth: 03,
+              expiryYear: 26,
+              color: Colors.blue[400],
+            ),
+            MyCard(
+              balance: 700,
+              cardNumber: 892234,
+              expiryMonth: 09,
+              expiryYear: 27,
+              color: Colors.green[400],
+            ),
+        ],
+      ),
+      ),
+          //3 buttons -> send //pay // bill
+        
+          //column-> stats +transaction
+        ],),
+      ),
     );
   }
 }
